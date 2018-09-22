3094ac3c538e8f5bf4de86c72ab0ab5f8acd3414
kaneta <kaneta1011@gmail.com>
2018-09-22 16:54:26 +0900

    use mdns

--- main.go
+++ main.go
@@ -1,37 +1,58 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"log"
+	"strings"
 
-	"github.com/evalphobia/google-home-client-go/googlehome"
+	"github.com/kaneta1992/google-home-client-go/googlehome"
 	"github.com/micro/mdns"
 )
 
-func main() {
-	// Make a channel for results and start listening
+const (
+	googleCastServiceName = "_googlecast._tcp"
+	googleHomeModelInfo   = "md=Google Home"
+)
+
+type GoogleHomeInfo struct {
+	Ip   string
+	Port int
+}
+
+func LookupHomeIP() []*GoogleHomeInfo {
 	entriesCh := make(chan *mdns.ServiceEntry, 4)
+
+	results := []*GoogleHomeInfo{}
 	go func() {
 		for entry := range entriesCh {
-			fmt.Printf("Got new entry: %v\n", entry)
+			log.Printf("[INFO] ServiceEntry detected: [%s:%d]%s", entry.AddrV4, entry.Port, entry.Name)
+			for _, field := range entry.InfoFields {
+				if strings.HasPrefix(field, googleHomeModelInfo) {
+					results = append(results, &GoogleHomeInfo{entry.AddrV4.String(), entry.Port})
+				}
+			}
 		}
 	}()
 
-	cli, err := googlehome.NewClientWithConfig(googlehome.Config{
-		Hostname: "192.168.0.2",
-		Lang:     "ja",
-		Accent:   "GB",
-	})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("ai")
-
-	// Change language
-	cli.Notify("かねたさんこんにちは、Googleです。")
-	// Start the lookup
-	googleCastServiceName := "_googlecast._tcp"
 	mdns.Lookup(googleCastServiceName, entriesCh)
 	close(entriesCh)
-	time.Sleep(time.Second * 5)
+
+	return results
+}
+
+func main() {
+
+	homes := LookupHomeIP()
+
+	for _, home := range homes {
+		cli, err := googlehome.NewClientWithConfig(googlehome.Config{
+			Hostname: home.Ip,
+			Lang:     "ja",
+			Accent:   "GB",
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		cli.Notify("かねたさんこんにちは、Googleです。")
+	}
 }
